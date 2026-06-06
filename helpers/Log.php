858dfde3939ca86906b364d37813ad2172237942
Mika <227877359+Mikaaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2026-06-05 22:10:47 -0300

    Refactorización de código y ajustes menores.

--- helpers/Log.php
+++ helpers/Log.php
@@ -1,30 +1,28 @@
 <?php
 
-class Log
-{
-    private static function write($level, $message)
-    {
+class Log {
+    private static function write($level, $message) {
         $logDir = __DIR__ . '/../log';
-        if (!is_dir($logDir)) {
+        if (is_dir($logDir) == false) {
             mkdir($logDir, 0755, true);
         }
+
         $file = $logDir . '/' . date('Y-m-d') . '.log';
         $line = sprintf("[%s] [%s] %s\n", date('H:i:s'), strtoupper($level), $message);
         file_put_contents($file, $line, FILE_APPEND | LOCK_EX);
     }
 
-    public static function info($message)
-    {
+    public static function info($message) {
         self::write('info', $message);
     }
 
-    public static function warning($message)
-    {
+    public static function warning($message) {
         self::write('warning', $message);
     }
 
-    public static function error($message)
-    {
+    public static function error($message) {
         self::write('error', $message);
     }
 }
+
+?>
