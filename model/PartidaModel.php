473cbb880c257702a0612ff0a42ba56d20abe279
Mika <227877359+Mikaaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2026-06-15 03:20:16 -0300

    Se agregaron los metodos obtenerPreguntaRandom y guardarPartida en el archivo PartidaModel.php.

---
+++ model/PartidaModel.php
@@ -0,0 +1,35 @@
+<?php
+
+class PartidaModel {
+    private $database;
+
+    public function __construct($database) {
+        $this->database = $database;
+    }
+
+    public function obtenerPreguntaRandom($idsUsados = []) {
+        if (empty($idsUsados) == true) {
+            $sql = "SELECT pre.*, cat.nombre, cat.color
+                    FROM preguntas pre
+                    JOIN categorias cat ON pre.categoria_id = cat.id
+                    ORDER BY RAND()
+                    LIMIT 1";
+        } else {
+            $ids = implode(",", $idsUsados);
+            $sql = "SELECT pre.*, cat.nombre, cat.color
+                    FROM preguntas pre
+                    JOIN categorias cat ON pre.categoria_id = cat.id
+                    WHERE pre.id NOT IN ($ids)
+                    ORDER BY RAND()
+                    LIMIT 1";
+        }
+
+        return $this->database->query($sql);
+    }
+
+    public function guardarPartida($usuario_id, $puntaje) {
+        return $this->database->execute("INSERT INTO partidas (usuario_id, puntaje) VALUES ($usuario_id, $puntaje)");
+    }
+}
+
+?>
